56736cb477c07d732367516adeec33e123efc26e
beratazgün <brtazgun@gmail.com>
2024-10-24 16:19:07 +0300

    Refactor exceptionFactory to include additional metadata for validation errors

--- apps/backend-service/src/core/exceptions/exception.factory.ts
+++ apps/backend-service/src/core/exceptions/exception.factory.ts
@@ -1,18 +1,27 @@
 import { ValidationError } from '@nestjs/common';
 
+export type MetadataKey = 'EXTRA_FIELDS';
+
 function exceptionFactory(errors: ValidationError[], parentField: string = '') {
   return errors.flatMap((error) => {
+    console.log(errors);
     const field = parentField
       ? `${parentField}.${error.property}`
       : error.property;
 
     if (error.constraints) {
-      return Object.values(error.constraints).map((constraint) => ({
-        field,
-        message: constraint,
-      }));
+      return Object.values(error.constraints).map((constraint) => {
+        return {
+          field,
+          isValid: false,
+          message: constraint,
+        };
+      });
     }
 
+    /**
+     * If the error has children, it means that the error is a nested object. Recursively call the exceptionFactory
+     */
     if (error.children && error.children.length > 0) {
       return exceptionFactory(error.children, field);
     }
